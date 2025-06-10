f94aeb817290640ea4b9537cbed4502e33af218a
Venkhat <117640419+VenkhatBalaji-FE@users.noreply.github.com>
2025-06-10 11:27:08 +0400

    fix: remove PageProps interface and inline type definition for Chat component

--- rag-chat-client/src/app/chat/[id]/page.tsx
+++ rag-chat-client/src/app/chat/[id]/page.tsx
@@ -3,12 +3,6 @@ import ChatSessionPage from "@/components/SesssionMessages";
 import { SessionType } from "@/hooks/useSession";
 import { redirect } from "next/navigation";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
 async function getData(id: string) {
   try {
     if (id) {
@@ -22,7 +16,7 @@ async function getData(id: string) {
   }
 }
 
-export default async function Chat({ params }: PageProps) {
+export default async function Chat({ params }: { params: { id: string } }) {
   const data = await getData(params.id);
   if (data) {
     return <ChatSessionPage sessionData={data} />;
